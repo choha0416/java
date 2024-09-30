f5d0abbdd9c548f1f29b41d540550ff14d5a719c
choha0416 <chohayun0416@gmail.com>
2024-09-30 17:29:57 +0900

    if else

--- src/ex930/IfElseExample.java
+++ src/ex930/IfElseExample.java
@@ -1,11 +1,11 @@
 package ex930;
-
+//if else예제 
 public class IfElseExample {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int score = 85;
-		
+		//if문에서 조건에 부합하지 않기때문에 else문으로 내려감
 		if(score >= 90) {
 			System.out.println("점수가 90보다 큽니다");
 			System.out.println("A등급입니다");
