fee0e077b7b4128c5b91ea9bc77d8953e2f15f89
choha0416 <chohayun0416@gmail.com>
2024-09-30 17:34:22 +0900

    무한 NaN

--- src/ex930/InfinityAndNaNCheckExample.java
+++ src/ex930/InfinityAndNaNCheckExample.java
@@ -1,5 +1,5 @@
 package ex930;
-
+//무한 낫어넘버
 public class InfinityAndNaNCheckExample {
 
 	public static void main(String[] args) {
@@ -10,8 +10,8 @@ public class InfinityAndNaNCheckExample {
 		double z = x / y;
 		//double z = x % y;
 		
-		System.out.println(Double.isInfinite(z));
-		System.out.println(Double.isNaN(z));
+		System.out.println(Double.isInfinite(z)); // 5를 0.0으로 나누면 무한이 나오기때문 참
+		System.out.println(Double.isNaN(z)); //숫자가 아니냐 물어보지만 숫자이기 때문에 거짓
 		
 		//잘못된 코드
 		System.out.println(z+2);
