53dff391a28f057ee2f79c34611314efaee7becd
choha0416 <chohayun0416@gmail.com>
2024-09-30 17:36:48 +0900

    isNaN

--- src/ex930/InputDataCheckNaNExample2.java
+++ src/ex930/InputDataCheckNaNExample2.java
@@ -13,7 +13,7 @@ public class InputDataCheckNaNExample2 {
 			System.out.println("NaN이 입력되어 처리할 수 없음");
 			val = 0.0;
 		}
-		
+		//isNaN으로 NaN인지 비교 NaN이 입력 되었기때문에 프린트문 실행
 		currentBalance += val;
 		System.out.println(currentBalance);
 	}
