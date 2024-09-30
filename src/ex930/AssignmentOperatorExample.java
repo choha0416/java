55cecac04ffae0d0da73cc509707f89664dd171a
choha0416 <chohayun0416@gmail.com>
2024-09-30 17:14:39 +0900

    a+=1

--- src/ex930/AssignmentOperatorExample.java
+++ src/ex930/AssignmentOperatorExample.java
@@ -5,7 +5,7 @@ public class AssignmentOperatorExample {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int result = 0;
-		result +=10;
+		result +=10; // result = result + 10 을 나타내나 result를 생략가능 result =+ 10이지만 +를 우선 처리하기때문에 result +=10로 표시
 		System.out.println("result="+ result);
 		
 		result -=5;
