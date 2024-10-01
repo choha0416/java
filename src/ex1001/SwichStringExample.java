8c474ab8e66bdc19684c54f7ed2454e6e33d7474
choha0416 <chohayun0416@gmail.com>
2024-10-01 17:36:01 +0900

    스위치

--- src/ex1001/SwichStringExample.java
+++ src/ex1001/SwichStringExample.java
@@ -1,12 +1,12 @@
 package ex1001;
-
+//스위치
 public class SwichStringExample {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String position = "과장";
 		
-		switch(position) {
+		switch(position) { //포지션에 과장이 들어가 있기때문에 case 과장의 프린트문을 실행후 swich문을 빠져나옴
 		case "부장":
 			System.out.println("700만원");
 			break;
