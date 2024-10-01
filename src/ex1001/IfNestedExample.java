a4a490826ac30482b52f2afd4c7a7fe176352afe
choha0416 <chohayun0416@gmail.com>
2024-10-01 17:18:47 +0900

    중첩 if문

--- src/ex1001/IfNestedExample.java
+++ src/ex1001/IfNestedExample.java
@@ -4,11 +4,11 @@ public class IfNestedExample {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int score = (int)(Math.random()*20)+81;
-		System.out.println("점수: " + score);
+		int score = (int)(Math.random()*20)+81; // 81~ 100의 수중 한개를 score에 보냄
+		System.out.println("점수: " + score); //score에 들어간 수를 출력
 		
 		String grade;
-		
+		//score값을 밑의 조건에 대입하여 실행
 		if(score>=90) {
 			if(score>=95) {
 				grade = "A+";
