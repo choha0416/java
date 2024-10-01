b10bb05bbe59b14d1f09b865442133ab11f05d16
choha0416 <chohayun0416@gmail.com>
2024-10-01 17:23:25 +0900

    //continue예제

--- src/ex1001/ContinueExample.java
+++ src/ex1001/ContinueExample.java
@@ -6,7 +6,7 @@ public class ContinueExample {
 		// TODO Auto-generated method stub
 		System.out.println("------짝수 출력------");
 		for(int i=1; i<=10; i++) {
-			if(i%2 !=0) { 
+			if(i%2 !=0) { //1부터 10중 i값을 2로 나눈값이 0이면 프린트문으로 즉 짝수일경우 출력
 				continue;// 컨티뉴가 실행될경우 i++로 가서 증감후 조건 실행
 			}
 			System.out.println(i);
