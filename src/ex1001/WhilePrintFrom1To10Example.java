f81ddf49d09e539c2388cce7f5a600a5995580f4
choha0416 <chohayun0416@gmail.com>
2024-10-01 12:30:41 +0900

    while문

---
+++ src/ex1001/WhilePrintFrom1To10Example.java
@@ -0,0 +1,22 @@
+package ex1001;
+
+public class WhilePrintFrom1To10Example {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+		int i = 1;
+		
+		while(i<=10) {
+			System.out.println(i);
+			i++;
+		}
+		*/
+		int i = 0;
+		while(i<=9) {
+			i++;
+			System.out.println(i);
+		}
+	}
+
+}
