ba7cf4f6de796703ed02577104a0fc1449603960
choha0416 <chohayun0416@gmail.com>
2024-10-01 12:08:59 +0900

    1부터100합

---
+++ src/ex1001/ForSumFrom1To100Example.java
@@ -0,0 +1,27 @@
+package ex1001;
+
+public class ForSumFrom1To100Example {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+		 * int sum = 0;
+		 * 
+		 * for(int i=1; i<=100; i++){
+		 * 	sum += i;
+		 * }
+		 * 
+		 * System.out.println("1~100 합 : " + sum);
+		 * */
+		
+		int sum= 0;
+		
+		int i = 0;
+		
+		for(i=1; i<=100; i++){
+			  	sum += i;
+		}
+		System.out.println("1~" +(i-1)+" 합 : " + sum);
+	}
+
+}
