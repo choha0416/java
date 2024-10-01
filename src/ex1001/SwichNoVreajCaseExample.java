0daef45121e169cf006ef5ad013d116e6e0167fb
choha0416 <chohayun0416@gmail.com>
2024-10-01 10:34:40 +0900

    Add files via upload

---
+++ src/ex1001/SwichNoVreajCaseExample.java
@@ -0,0 +1,27 @@
+package ex1001;
+
+public class SwichNoVreajCaseExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//8에서11사이 정수얻기
+		int time = (int)(Math.random()*4)+8;
+		
+		System.out.println("[현재시간: "+time+" 시]");
+		
+		switch(time) {
+		case 8:
+			System.out.println("출근합니다.");
+			break;
+		case 9:
+			System.out.println("회의를 합니다.");
+			break;
+		case 10:
+			System.out.println("업무를 봅니다.");
+			break;
+		default:
+			System.out.println("외근을 나갑니다.");
+		}
+	}
+
+}
