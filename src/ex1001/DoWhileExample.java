28ac428a870d29a3dc6f0b46fa66a89b6909c36a
choha0416 <chohayun0416@gmail.com>
2024-10-01 17:25:21 +0900

    do while문

--- src/ex1001/DoWhileExample.java
+++ src/ex1001/DoWhileExample.java
@@ -12,11 +12,11 @@ public class DoWhileExample {
 		Scanner scanner = new Scanner(System.in);
 		String inputString;
 		
-		do {
+		do { //do while문이기 때문에 일단 중괄호 안의 내용을 실행후 while의 조건을따짐
 			System.out.print(">");
 			inputString = scanner.nextLine();
 			System.out.println(inputString);
-		} while(! inputString.equals("q"));
+		} while(! inputString.equals("q")); //입력받은 값이q가 아닐경우 다시 중괄호 안을 처리 q일경우 반복 종료후 밑의 프린트문으로
 		
 		System.out.println();
 		System.out.println("프로그램 종료");
