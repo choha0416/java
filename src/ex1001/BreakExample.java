cbff1f0e179785233e1927e332290e62d4ebb9e6
choha0416 <chohayun0416@gmail.com>
2024-10-01 17:20:57 +0900

    break문

--- src/ex1001/BreakExample.java
+++ src/ex1001/BreakExample.java
@@ -5,9 +5,9 @@ public class BreakExample {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		while(true) {
-			int num = (int)(Math.random()*6)+1;
+			int num = (int)(Math.random()*6)+1; //1에서 6까지의수중 한가지를 num에 보냄
 			System.out.println(num);
-			if(num == 6) {
+			if(num == 6) { //num이 6이나오면 while문 종료 아닐경우 6이 나올때 까지 계속 print문으로 num값을 출력
 				break;
 			}
 		}
