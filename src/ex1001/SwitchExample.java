a13c2faae0fd41985700eca5930cd90148666457
choha0416 <chohayun0416@gmail.com>
2024-10-01 17:37:54 +0900

    스위치문

--- src/ex1001/SwitchExample.java
+++ src/ex1001/SwitchExample.java
@@ -6,7 +6,7 @@ public class SwitchExample {
 		// TODO Auto-generated method stub
 		int num = (int)(Math.random()*6)+1;
 		
-		switch(num) {
+		switch(num) { //랜덤 메소드에서 나온값을 num에 넣고 그값을 case와 비교 후 해당 프린트문 실행후 break문으로 switch문을 빠져나옴
 		case 1:
 			System.out.println("1번이 나왔습니다.");
 			break;
