f0515b1042456bce2316ae3e62c5ba0b007af94f
choha0416 <chohayun0416@gmail.com>
2024-10-02 17:02:50 +0900

    문자열 대체

--- src/ex1002/ReplaceExample.java
+++ src/ex1002/ReplaceExample.java
@@ -1,5 +1,5 @@
 package ex1002;
-
+//replace()메소드는 기존 문자열은 그대로 두고 대체한 새로운 문자열을 리턴
 public class ReplaceExample {
 
 	public static void main(String[] args) {
@@ -12,3 +12,4 @@ public class ReplaceExample {
 	}
 
 }
+// 자바 문자열은 불변입니다. 자바 문자열은 String입니다 해당 문장의 자바를 java로 바꿈
