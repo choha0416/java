f3821c6a6053d4cc83af8a4d7a31c2eb0765e0e1
choha0416 <chohayun0416@gmail.com>
2024-10-02 17:08:52 +0900

    문자열 분리

--- src/ex1002/SplitExample.java
+++ src/ex1002/SplitExample.java
@@ -1,12 +1,12 @@
 package ex1002;
-
+//구분자가 있는 여러 개의 문자열을 분리할 때 사용
 public class SplitExample {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String board = "1,자바 학습, 탐조 타입 스트링을 학습합니다, 김성민";
 		
-		String[] tokens = board.split(",");
+		String[] tokens = board.split(","); //,를 기준으로 문자를 분리함 ,가 3개이기때문에 4개의 공간이 생김
 		
 		System.out.println("번호" + tokens[0]);
 		System.out.println("제목" + tokens[1]);
