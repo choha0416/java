22bffe596c658898f6c54bac56043e8efeaeb55e
choha0416 <chohayun0416@gmail.com>
2024-10-02 17:04:34 +0900

    문자열 잘라내기

--- src/ex1002/SubStringExample.java
+++ src/ex1002/SubStringExample.java
@@ -1,15 +1,15 @@
 package ex1002;
-
+//문자열 에서 특정 위치의 문자열을 잘라내어 가져오고 싶을때 사용
 public class SubStringExample {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String ssn = "950416-1234567";
 		
-		String firstNum = ssn.substring(0,6);
+		String firstNum = ssn.substring(0,6); //인덱스 0포함 6미포함임으로  950416까지 출력
 		System.out.println(firstNum);
 		
-		String secondNum = ssn.substring(7);
+		String secondNum = ssn.substring(7); //숫자가 하나일경우 해당수 포함 끝까지 불러오기때문에 1234567이 출력
 		System.out.println(secondNum);
 		
 	}
