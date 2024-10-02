096b23d39cacb2df1286bbbe19ed06de8518cb2f
choha0416 <chohayun0416@gmail.com>
2024-10-02 17:01:02 +0900

    문자열 길이

--- src/ex1002/LengthExample.java
+++ src/ex1002/LengthExample.java
@@ -1,12 +1,12 @@
 package ex1002;
-
+//문자열에서 문자의 개수를 얻고 싶을때 length()메소드를 사용한다
 public class LengthExample {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String ssn = "9504161234567";
 		int length = ssn.length();
-		if(length == 13) {
+		if(length == 13) { //주민번호의 경우 13자리이기 때문에 length가 13일경우 if문의 처리문 실행 아닐경우 else문의 처리문 실행
 			System.out.println("주민번호 자릿수가 맞음");
 		} else {
 			System.out.println("주민번호 자릿수가 틀림");
