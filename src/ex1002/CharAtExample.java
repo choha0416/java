58683cd176706368860efa6b7251fa34ba780f37
choha0416 <chohayun0416@gmail.com>
2024-10-02 16:58:59 +0900

    문자열 추출

--- src/ex1002/CharAtExample.java
+++ src/ex1002/CharAtExample.java
@@ -1,11 +1,11 @@
 package ex1002;
-
+//charAt()메소드 인덱스의 문자를 리턴에 특정 위치의 문자를 얻는다
 public class CharAtExample {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String ssn = "9504161234567";
-		char sex = ssn.charAt(6);
+		char sex = ssn.charAt(6); //index6번의 수를 얻기때문에 1 즉 성별부분의 값을 얻을 수 있다
 		switch (sex) {
 		case '1':
 		case '3':
