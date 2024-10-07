2bc4b3eb7915217d2aa3fc5c854a02f9d76e2fa9
choha0416 <chohayun0416@gmail.com>
2024-10-07 17:01:17 +0900

    다른 생성자 생성 코드 줄이기

--- src/ex1007/Car.java
+++ src/ex1007/Car.java
@@ -1,10 +1,10 @@
 package ex1007;
-
+//다른 생성자 생성 코드 줄이기
 public class Car {
-String company = "현대자동차";
-String model;
-String color;
-int maxSpeed;
+String company = "현대자동차"; //명시적 초기화
+String model; //생성자 초기화
+String color; //생성자 초기화 
+int maxSpeed; //생성자 초기화
 
 //생성자
 Car(){
