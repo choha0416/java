5c0f956c3ac000a908cf642740d5f0240a76feb1
choha0416 <chohayun0416@gmail.com>
2024-10-07 17:26:39 +0900

    정적 멤버사용

--- src/ex1007/StaticCaculator.java
+++ src/ex1007/StaticCaculator.java
@@ -1,9 +1,9 @@
 package ex1007;
 
 public class StaticCaculator {
-	static double pi = 3.14159;
+	static double pi = 3.14159; 
 	
-	static int plus(int x, int y) {
+	static int plus(int x, int y) { //스태틱으로 생성될 경우 클래스명.메소드명(변수)로 사용가능 new로 객체 생성할 필요가 없음
 		return x + y;
 	}
 	
