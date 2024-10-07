ee9f716d16ab79a7f93122c1b1e6af9d68905282
choha0416 <chohayun0416@gmail.com>
2024-10-07 12:29:38 +0900

    정적 초기화 블록

---
+++ src/ex1007/Television.java
@@ -0,0 +1,11 @@
+package ex1007;
+
+public class Television {
+	static String company = "samsung";
+	static String model = "lcd";
+	static String info;
+	
+	static {
+		info = company +"-"+model;
+	}
+}
