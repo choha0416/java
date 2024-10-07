156484da58e9cc29b608a066f6dcd84c476610cc
choha0416 <chohayun0416@gmail.com>
2024-10-07 17:24:38 +0900

    매개변수의 수를 모를경우

--- src/ex1007/Computer.java
+++ src/ex1007/Computer.java
@@ -1,7 +1,7 @@
 package ex1007;
 
 public class Computer {
-	int sum1(int[] values) {
+	int sum1(int[] values) { // 배열형
 		int sum = 0;
 		for(int i=0; i<values.length;i++) {
 			sum+=values[i];
@@ -9,7 +9,7 @@ public class Computer {
 		return sum;
 	}
 	
-	int sum2(int ... values) {
+	int sum2(int ... values) { // ex int result = mycom.sum(1,2,3) 값이 하나씩 추가 공간을 한번씩 총 3번생성 
 		int sum = 0;
 		for(int i=0; i<values.length;i++) {
 			sum+=values[i];
