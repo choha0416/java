3a7e792efeddafe2136831245c619aeff953f3df
choha0416 <chohayun0416@gmail.com>
2024-10-07 14:00:37 +0900

    필드 선언과 초기화

---
+++ src/ex1007/Person.java
@@ -0,0 +1,12 @@
+package ex1007;
+
+public class Person {
+	final String Nation = "kores";
+	final String ssn;
+	String name;
+	
+	public Person(String ssn, String name) {
+		this.ssn = ssn;
+		this.name = name;
+	}
+}
