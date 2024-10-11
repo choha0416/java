6336f738d0b1dcec39ae8fb189cde65bdd2645ee
choha0416 <chohayun0416@gmail.com>
2024-10-11 12:15:06 +0900

    해시코드 생성

---
+++ src/ex1011/HashCodeExample.java
@@ -0,0 +1,28 @@
+package ex1011;
+
+import java.util.*;
+
+public class HashCodeExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Student s1 = new Student(1,"홍길동");
+		Student s2= new Student(1,"홍길동");
+		System.out.println(s1.hashCode());
+		System.out.println(Objects.hashCode(s2));
+	}
+	
+	static class Student{
+		int sno;
+		String name;
+		Student(int sno, String name){
+			this.sno = sno;
+			this.name = name;
+		}
+		@Override
+		public int hashCode() {
+			return Objects.hash(sno,name);
+		}
+	}
+
+}
