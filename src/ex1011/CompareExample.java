737a19ba28fa60cf1f43384d9432fe065e23609e
choha0416 <chohayun0416@gmail.com>
2024-10-11 11:19:51 +0900

    비교자

---
+++ src/ex1011/CompareExample.java
@@ -0,0 +1,38 @@
+package ex1011;
+import java.util.Comparator;
+
+import java.util.Objects;
+
+public class CompareExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Student s1 = new Student(1);
+		Student s2 = new Student(1);
+		Student s3 = new Student(2);
+		
+		int result = Objects.compare(s1, s2,new StudentComparartor());
+		System.out.println(result);
+		result = Objects.compare(s1, s3,new StudentComparartor());
+		System.out.println(result);
+	}
+	
+	static class Student{
+		int sno;
+		Student(int sno){
+			this.sno=sno;
+		}
+	}
+	
+	static class StudentComparartor implements Comparator<Student>{
+		@Override
+		public int compare(Student a, Student b) {
+			/*if(a.sno<b.sno) return -1;
+			 * else if (a.sno==b.sno) return 0;
+			 * else return 1;
+			 */
+			return Integer.compare(a.sno, b.sno);// 앞에서 뒷수를 뺌
+		}
+	}
+
+}
