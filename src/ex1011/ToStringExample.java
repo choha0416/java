9fa8622852a5f3f88948c413faba657e86a3211e
choha0416 <chohayun0416@gmail.com>
2024-10-11 12:38:43 +0900

    객체 문자 정보

---
+++ src/ex1011/ToStringExample.java
@@ -0,0 +1,15 @@
+package ex1011;
+import java.util.*;
+public class ToStringExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str1 = "홍길동";
+		String str2 = null;
+		
+		System.out.println(Objects.toString(str1));
+		System.out.println(Objects.toString(str2));
+		System.out.println(Objects.toString(str2,"이름이 없습니다"));
+	}
+
+}
