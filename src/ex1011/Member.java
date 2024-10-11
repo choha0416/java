4e44a6d3e1d3813d07911fc1867ba8287deacbf4
choha0416 <chohayun0416@gmail.com>
2024-10-11 10:37:11 +0900

    Add files via upload

---
+++ src/ex1011/Member.java
@@ -0,0 +1,23 @@
+package ex1011;
+
+public class Member {
+	public String id;
+	
+	public Member(String id) {
+		this.id = id;
+	} 
+	
+	@Override
+	public boolean equals(Object obj) {
+		if(obj instanceof Member) {
+			Member member = (Member) obj;
+			if(id.equals(member.id)) {
+				System.out.println("id= "+id+" member.id="+member.id);
+				return true;
+			}else {
+				System.out.println("id="+id+" member.id="+member.id);
+			}
+		}
+		return false;		
+	}
+}
---
+++ src/ex1011/SmartPhoneExample.java
@@ -0,0 +1,16 @@
+package ex1011;
+
+public class SmartPhoneExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SmartPhone myPhone = new SmartPhone("구글","안드로이드");
+		
+		String strobj = myPhone.toString();
+		System.out.println(strobj);
+		
+		System.out.println(myPhone);
+		
+	}
+
+}
