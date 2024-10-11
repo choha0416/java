ff2d3d5a8a4b57e29cc8fdb9f287cc77d79fd890
choha0416 <chohayun0416@gmail.com>
2024-10-11 12:30:28 +0900

    null 여부조사

---
+++ src/ex1011/NullExample.java
@@ -0,0 +1,33 @@
+package ex1011;
+
+import java.util.*;
+
+public class NullExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str1 = "홍길동";
+		String str2 = null;
+		
+		System.out.println(Objects.requireNonNull(str1));
+		
+		try {
+			String name = Objects.requireNonNull(str2);
+		} catch(Exception e) {
+			System.out.println(e.getMessage());
+		}
+		
+		try {
+			String name = Objects.requireNonNull(str2,"이름이 없습니다");
+		} catch(Exception e) {
+			System.out.println(e.getMessage());
+		}
+		
+		try {
+			String name = Objects.requireNonNull(str2,()->"이름이 없습니다2");
+		} catch(Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
+
+}
