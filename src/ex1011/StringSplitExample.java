80c0fe9a3c37c6198c6f6882fc6059063fae3032
choha0416 <chohayun0416@gmail.com>
2024-10-11 15:09:53 +0900

    문자열 분리\

---
+++ src/ex1011/StringSplitExample.java
@@ -0,0 +1,15 @@
+package ex1011;
+
+public class StringSplitExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String text = "홍길동&이수홍,박연수,김자바-최명호";
+		String[] names = text.split("&|,|-");
+		
+		for(String name : names) {
+			System.out.println(name);
+		}
+	}
+
+}
