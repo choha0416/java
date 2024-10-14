14c686c2de26e2c76b38d58e0d2f79dda540fe54
choha0416 <chohayun0416@gmail.com>
2024-10-14 16:20:13 +0900

    와일드타입 매개변수

---
+++ src/ex1014/HighStudent.java
@@ -0,0 +1,34 @@
+package ex1014;
+
+public class HighStudent extends Student {
+	public HighStudent(String name) {
+		super(name);
+	}
+}
+
+class Person{
+	private String name;
+	
+	public Person(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public String toString() {
+		return name;
+	}
+}
+
+class Student extends Person{
+	public Student(String name) {
+		super(name);
+	}
+}
+
+class Worker extends Person{
+	public Worker(String name) {
+		super(name);
+	}
+}
