7c816c3934de5b028b3d074443f45994f9bf9618
choha0416 <chohayun0416@gmail.com>
2024-10-15 10:48:42 +0900

    제네릭 타입 사용 클래스

---
+++ src/ex1015/ChildProduct.java
@@ -0,0 +1,11 @@
+package ex1015;
+
+public class ChildProduct<T,M,C> extends Product<T,M> {
+	private C company;
+	public C getCompany() {
+		return this.company;
+	}
+	public void setCompany(C company) {
+		this.company = company;
+	}
+}
